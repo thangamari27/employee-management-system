feb4b6041bb2bd9effd47f264beb0a6c5beece45
thangamari27 <144316551+thangamari27@users.noreply.github.com>
2025-04-23 00:21:45 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Wrapper from "./componets/container";
+import Heading from "./componets/heading";
+
+function App() {
+
+
+  return (
+    <>
+  <Wrapper>
+    <Heading />
+  </Wrapper>
+    </>
+  )
+}
+
+export default App
---
+++ src/componets/Container.jsx
@@ -0,0 +1,13 @@
+
+
+const Wrapper = ({Children}) => {
+    return (
+        <>
+            <div> 
+                <h1>HI</h1>
+                {Children}
+            </div>
+        </>
+    );
+}
+export default Wrapper;
