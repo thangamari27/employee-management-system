efefffd22f3578aa486d77ea7feb82329b1a3acb
thangamari27 <thangamari616@gmail.com>
2025-08-05 21:01:41 +0530

    update the code in the employee management system

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDwUvTA5IjOhnbmWd7bmQwgEunFaZdqx2c",
-  authDomain: "emsproject-6fcec.firebaseapp.com",
-  projectId: "emsproject-6fcec",
-  storageBucket: "emsproject-6fcec.firebasestorage.app",
-  messagingSenderId: "325204426181",
-  appId: "1:325204426181:web:f9fc39c115abba9179dfac"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
